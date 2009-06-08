8fb60f2fb075ba590d3ead135ca33142bcb07282
Justin Azoff <justin@bouncybouncy.net>
2009-06-07 23:15:58 -0400

    new page

---
+++ programs.php
@@ -0,0 +1,40 @@
+<? 
+$title = "Programs";
+include("header.inc");
+?>
+<div id="main">
+<h1> Programs and Camps </h1>
+        
+<h2>Adults</h2>
+<p> We offer competitive, intermediate, and recreational programs for
+adults of all ages.</p>
+<ul>
+  <li><a href="/html/competitive.html">Competitive</a> </li>
+  <li><a href="/html/coached_intermediate.html">Coached / Intermediate</a> </li>
+  <li><a href="/html/recreational.html">Recreational</a> </li>
+  <li><a href="/html/visiting_rowers.html">Visiting Rowers</a> </li>
+</ul>
+
+<h2> Juniors </h2>
+<p> Young athletes row the Hudson, are instructed in the sport and
+develop competitively.</p>
+<ul>
+
+    <li><a href="/html/junior_programs.html">Junior Program Overview</a></li>
+    <li><a href="/html/high_school_rowing.html">High School Rowing</a></li>
+    <li><a href="/html/modified_rowing.html">Modified Rowing</a></li>
+    <li><a href="/html/summer_programs.html">Summer Programs</a></li>
+</ul>
+
+<h2> Learn to Row </h2>
+<p> We offer learn to row programs and camps for both youth and
+adults.</p>
+
+<ul>
+    <li><a href="/html/adults.html">Adults</a></li>
+    <li><a href="/html/summer_programs.html">Juniors</a></li>
+    <li><a href="/html/national_learn_to_row_day.html">National Learn to Row Day</a></li>
+</ul>
+
+</div>
+<? include("footer.inc"); ?>
