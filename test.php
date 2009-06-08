fdae123c49ca4938e0d34283b5c218f0e718d3fb
Justin Azoff <justin@bouncybouncy.net>
2009-06-07 22:40:21 -0400

    working

---
+++ test.php
@@ -0,0 +1,19 @@
+<? 
+$title = "Home";
+include("header.inc");
+?>
+<div id="main">
+<h1> Welcome to Albany Rowing Center! </h1>
+        
+<p>
+Albany Rowing Center (ARC) is a not-for-profit organization that fosters
+interest in and provides opportunity for rowing in New York's Capital
+District.
+</p>
+
+<p>
+ARC rowers represent all ages (from middle school students on up) and a
+wide range of rowing abilities (from novice to experienced). 
+</p>
+</div>
+<? include("footer.inc"); ?>
