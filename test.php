051b6bd042c32f89afc1dd74c943f8a2d95f3955
Justin Azoff <justin@bouncybouncy.net>
2009-06-07 22:55:00 -0400

    first test page
    update css colors to match logo
    fix all validation issues

--- test.php
+++ test.php
@@ -15,5 +15,57 @@ District.
 ARC rowers represent all ages (from middle school students on up) and a
 wide range of rowing abilities (from novice to experienced). 
 </p>
+
+<h2>Rowing Programs and Camps</h2>
+<table border="0">
+<tr>
+<td valign="top">
+<img src="/assets/images/adults.gif" alt="Adults"/>
+<h3>Adults</h3>
+<p> We offer competitive, intermediate, and recreational programs for
+adults of all ages.</p>
+
+<ul>
+  <li><a href="/html/competitive.html">Competitive</a> </li>
+  <li><a href="/html/coached_intermediate.html">Coached / Intermediate</a> </li>
+  <li><a href="/html/recreational.html">Recreational</a> </li>
+  <li><a href="/html/visiting_rowers.html">Visiting Rowers</a> </li>
+</ul>
+
+</td>
+
+<td valign="top">
+<img src="/assets/images/juniors.gif" alt="Juniors"/>
+<h3> Juniors </h3>
+<p> Young athletes row the Hudson, are instructed in the sport and
+develop competitively.</p>
+<ul>
+
+    <li><a href="/html/junior_programs.html">Junior Program Overview</a></li>
+    <li><a href="/html/high_school_rowing.html">High School Rowing</a></li>
+    <li><a href="/html/modified_rowing.html">Modified Rowing</a></li>
+    <li><a href="/html/summer_programs.html">Summer Programs</a></li>
+</ul>
+</td>
+
+<td valign="top">
+<img src="/assets/images/l2r.gif" alt="Learn To Row"/>
+<h3> Learn to Row </h3>
+<p> We offer learn to row programs and camps for both youth and
+adults.</p>
+
+<ul>
+    <li><a href="/html/adults.html">Adults</a></li>
+    <li><a href="/html/summer_programs.html">Juniors</a></li>
+    <li><a href="/html/national_learn_to_row_day.html">National Learn to Row Day</a></li>
+</ul>
+
+</td>
+
+</tr>
+</table>
+
+
+
 </div>
 <? include("footer.inc"); ?>
