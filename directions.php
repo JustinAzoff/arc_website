c60c3936883cc95124dfe647e0a9d7bf5739e5ea
Justin Azoff <justin@bouncybouncy.net>
2009-06-07 23:25:02 -0400

    new page

---
+++ directions.php
@@ -0,0 +1,25 @@
+<? 
+$title = "Directions";
+include("header.inc");
+?>
+<div id="main">
+<h1> Directions </h1>
+
+<h2>From ...</h2>
+<ul>
+    <li> Blah blah </li>
+    <li> Blah blah </li>
+</ul>
+
+<h2>From ...</h2>
+<ul>
+    <li> Blah blah </li>
+    <li> Blah blah </li>
+</ul>
+        
+<h2>Map</h2>
+
+<iframe width="640" height="480" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?q=albany+rowing+center&amp;oe=utf-8&amp;client=firefox-a&amp;ie=UTF8&amp;hl=en&amp;t=h&amp;cid=8539449587296303085&amp;ll=42.66363,-73.750706&amp;spn=0.030295,0.054932&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe><br /><small><a href="http://maps.google.com/maps?q=albany+rowing+center&amp;oe=utf-8&amp;client=firefox-a&amp;ie=UTF8&amp;hl=en&amp;t=h&amp;cid=8539449587296303085&amp;ll=42.66363,-73.750706&amp;spn=0.030295,0.054932&amp;z=14&amp;iwloc=A&amp;source=embed" style="color:#0000FF;text-align:left">View Larger Map</a></small>
+
+</div>
+<? include("footer.inc"); ?>
