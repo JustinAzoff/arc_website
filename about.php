7d078882c64099de8de36bdd5dc2496f8fbaa682
Justin <justin@bouncybouncy.net>
2009-06-08 23:39:29 -0400

    more pages

---
+++ about.php
@@ -0,0 +1,14 @@
+
+<? 
+$title = "About ARC";
+include("header.inc");
+?>
+<div id="main">
+<h1> <?= $title ?> </h1>
+
+<p>
+...
+</p>
+
+</div>
+<? include("footer.inc"); ?>
---
+++ contact.php
@@ -0,0 +1,50 @@
+
+<? 
+$title = "Contact Us";
+include("header.inc");
+?>
+<div id="main">
+<h1> <?= $title ?> </h1>
+
+<h2> Mailing Address </h2>
+<pre>
+    Albany Rowing Center
+    PO Box 857
+    Albany, NY 12201-0857
+</pre>
+ 
+<h2> Boathouse </h2>
+
+Our boathouse is located along the historic Albany Rensselaer Riverfront Park,
+just north of the railroad bridge. <a href="directions.php">Directions to the Boathouse</a>.
+                                                        
+    
+
+<h2> Email Contacts </h2>
+
+<h3> Adult Programs </h3>
+<ul>
+    <li>Non-Competitive - <a href="mailto:rec_rowing@albanyrowingcenter.org">rec_rowing@albanyrowingcenter.org</a></li>
+    <li>Adult Learn to Row - <a href="mailto:learn2row@albanyrowingcenter.org">learn2row@albanyrowingcenter.org</a></li>
+    <li>Adult Competitive - <a href="mailto:mastersrowing@albanyrowingcenter.org">mastersrowing@albanyrowingcenter.org</a></li>
+</ul>
+
+<h3> Junior Programs </h3>
+<ul>
+    <li>Junior Programs - <a href="mailto:dquinn5@nycap.rr.com">dquinn5@nycap.rr.com</a></li>
+</ul>
+
+<h3> Head of the Hudson Regatta: </h3>
+<ul>
+    <li>Head of the Hudson Regatta - <a href="mailto:jrtauzel@yahoo.com">jrtauzel@yahoo.com</a></li>
+</ul>
+    
+
+<h3> All Other Inquiries </h3>
+<ul>
+    <li><a href="mailto:info@albanyrowingcenter.org">info@albanyrowingcenter.org</a></li>
+</ul>    
+    
+
+</div>
+<? include("footer.inc"); ?>
