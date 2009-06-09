c6ad9e8b294a7e202ac33c49334dbb527fccd544
Justin <justin@bouncybouncy.net>
2009-06-08 23:30:08 -0400

    new page

---
+++ junior_programs.php
@@ -0,0 +1,105 @@
+
+<? 
+$title = "Junior Rowing";
+include("header.inc");
+?>
+<div id="main">
+<h1> Junior Rowing </h1>
+
+
+<p>
+Rowing is an amazing sport that never fails to challenge the body, mind, and
+soul of its participants. At Albany Rowing Center, junior rowers gain strength,
+perseverance, and passion for the sport as they are coached to perform their
+best and perhaps discover that their best far exceeds their original
+expectations.
+</p>
+
+<p>
+Junior rowing at ARC has become very competitive at New York State Scholastic
+Rowing Association Championships and at the USRowing National Youth
+Championships and other regional and national events. Many ARC rowers
+participate in USRowing Olympic Selection Camps, go on to row in college and
+compete at national and international events.
+</p>
+
+<p>
+ARC recognizes that excellence does not appear but must be fostered in each
+rower so that individual and team goals can be set and accomplished. Rowers of
+all levels, from the beginner to the experienced, are coached to reach their
+potential. Juniors ages 11-18 are all given the opportunity to learn to row and
+compete. 
+</p>
+
+<h2> Modified Rowing (Mods) </h2>
+<a href="modified_rowing.php">More about MODS</a>
+
+<p>
+ARC Junior rowers in 6th-8th grade may participate with the ARC Modified Team
+beginning in the Spring of 2009. No prior rowing experience is required. We
+intend to focus on the creation of a fun and safe opportunity to learn the
+sport of rowing and the basics of competition.   Modified Rowing is anticipated
+to grow, and more opportunities for competition will arise.  Until then this
+program may be thought of as a gateway for rowing hopefuls.
+</p>
+
+<p>
+The Modified Program is a first step into competitive rowing.  Consideration of
+modified rowers needs and expectations will be given. Rowers will emerge with a
+good foundation of skill and knowledge. Highly competitive Modified Rowers
+(MODS) may be eligible to row on the High School Team in 8th grade at the
+discretion of the Coach. Skill development in MODS will emphasize safety,
+technique, coordination, synchronization, flexibility, balance and strength.
+</p>
+
+<p>
+Spring Season: 3/30/09-5/30/09 <br/>
+Practice: M-TH 4-6PM <br/>
+Regattas: Friday Afternoons <br/>
+Fees - Rowers: $310 Coxswains: $100
+</p>
+
+<h2> High School Rowing </h2>
+    
+<p>
+High School rowers, from Novice to Varsity, will row competitively in the
+Spring and Fall. Non-competitive rowing is an option only in summer.
+Competitive rowers are divided into appropriate groups and boats based on their
+performance, erg times and commitment levels. Each group will focus on
+developing technique and fitness. 
+</p>
+
+<p>
+All rowers in their first year of rowing are novice rowers. These rowers work
+on improving technique, erg scores and general fitness. Novices row in some
+competitive regattas. Rowers are assessed regularly and evaluated for moving
+into the Varsity group. The Varsity group includes those who have achieved
+criteria set by the head coach, including reaching competitive erg scores,
+skill levels and commitment levels. This group will practice six times a week
+and compete at regional and national championships. 
+</p>
+
+<p>
+Coxswains - ARC also offers students the opportunity to learn coxswain skills.
+The coxswain is an essential part of sweep rowing, as essential as the rowers
+themselves. The job of the coxswain is to steer the boat, help the crew keep
+the cadence, calls out instructions and the stroke rating. The coxswain must be
+a good communicator.
+</p>
+<p>
+Spring Season: 3/16/09-5/30/09 <br/>
+Practice: M-Fr 4-6:15 PM Sat 9-11AM <br/>
+Regattas: See Schedule <br/>
+Fees - Rowers: $475 Coxswains: $100
+</p>
+
+<h2> Questions </h2>
+
+<ul>
+<li> Donna Quinn (Holloway) - Junior Program Coordinator - (518) 479-0469 - <a href="mailto:dquinn5@nycap.rr.com">dquinn5@nycap.rr.com</a> </li>
+<li> Drue Spallholz - Head Coach - (518) 312-7485 - <a href="mailto:druespalz@hotmail.com">druespalz@hotmail.com</a> </li>
+</ul>
+
+
+</div>
+<? include("footer.inc"); ?>
