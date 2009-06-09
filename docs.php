c787ab1594a6c4dca595ffe4752e0ee230c25d93
Justin <justin@bouncybouncy.net>
2009-06-08 23:47:59 -0400

    docs page

---
+++ docs.php
@@ -0,0 +1,26 @@
+
+<? 
+$title = "Forms and Documents";
+include("header.inc");
+?>
+<div id="main">
+<h1> <?= $title ?> </h1>
+
+<p>
+Important forms and documents for Albany Rowing Center. Forms require Adobe Acrobat Reader.
+</p>
+
+<ul>
+    <li><a href="/docs/Spring_09_Mods_Clothing_Info.pdf">Spring 2009 Mods Clothing Info</a></li>
+    <li><a href="/docs/Spring_09_Order_Form.pdf">Spring 2009 Clothing Order Form</a></li>
+    <li><a href="/docs/2009_USRA_Waiver.pdf">2009 USRA Waiver</a></li>
+    <li><a href="/docs/2009_ARC_Adult_Membership_Form.pdf">2009 ARC Adult Membership Form</a></li>
+    <li><a href="http://www.albanyrowingcenter.org/docs/L2R_Adult%20Reg_09.pdf">2009 ARC Adult Learn to Row Form</a></li>
+</ul>
+
+<p>
+<a href="http://get.adobe.com/reader/?promoid=BUIGO"><img id="Picture5" height="39" width="158" src="../assets/images/get_adobe_reader.png" border="0" alt="get_adobe_reader" title="get_adobe_reader"></a>
+</p>
+
+</div>
+<? include("footer.inc"); ?>
