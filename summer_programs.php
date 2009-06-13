ab2e4b0197874cdb233ba544385a2d16bdb508b0
Justin <justin@bouncybouncy.net>
2009-06-12 23:11:08 -0400

    new page

---
+++ summer_programs.php
@@ -0,0 +1,79 @@
+
+<? 
+$title = "Summer Camp 2009";
+include("header.inc");
+?>
+<div id="main">
+<h1> <?= $title ?> </h1>
+
+<p>
+The Albany Rowing Center Learn to Row programs offer the experience of rowing
+to interested youth entering grades 6 and above. The Learn to Row Program
+permits young athletes to learn about the sport of rowing through
+participation. Students will learn water and boat safety, stroke mechanics on
+the ergometer and the basic techniques of sweep rowing. While the majority of
+time is spent on the water, there is also dry land training and education about
+boats and equipment.
+</p>
+
+<h1> Swim Test </h1>
+
+<p>
+The swim test can be taken at ANY POOL where a lifeguard is present.  We do not
+administer swim tests at the boathouse. Please print out the
+<a href="/docs/2009_Junior_Camp_Reg.pdf">registration form</a> and have a
+lifeguard sign it. You can either mail the form to Albany Rowing Center OR
+bring it to your first day of class. You may not be boated without the swim
+test.
+</p>
+
+<h1> Requirements </h1>
+
+<ul>
+    <li> Completed registration packet </li>
+    <li> Payment in full for 1st session </li>
+    <li> Swim test certification </li>
+    <li> Must be entering Middle School </li>
+    <li> 6th grade or older </li>
+</ul>
+
+<h1> What to Bring... </h1>
+<ul> 
+    <li> Sunscreen </li>
+    <li> Refillable water bottle </li>
+    <li> Change of clothing (sneakers, socks, and snacks are all necessary items to bring) </li>
+</ul>
+
+<h1> Schedule and Time </h1>
+<p>
+Camp Time is 9 am – 11:30 am, Monday through Friday 
+</p>
+
+<ul>
+    <li> Session 1 - June 29th - July 10th </li>
+    <li> Session 2 - July 13th - July 24th </li>
+    <li> Session 3 - July 27th - August 7th </li>
+</ul>
+
+<h1> Fees </h1>
+<p>
+$195 per session.  (There is a discount for a child in more than one session: $370 for 2 and $520 for all 3)
+</p>
+
+<h1> Registration </h1>
+
+<p>
+Registration for 2009 Junior Learn to Row at Albany Rowing Center is simple.
+Please complete the 2009 Junior Learn to Row Reqistration and mail to:
+
+<pre>
+Organization of Adirondack Rowers and Scullers
+Junior Registration
+Albany Rowing Center C/O Bierman
+111 Read Road, Glenmont, NY 12077 
+</pre>
+</p>
+
+
+</div>
+<? include("footer.inc"); ?>
