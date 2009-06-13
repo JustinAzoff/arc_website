5797dc59d82a4eb4ce9788db3d4dad863a585dc6
Justin <justin@bouncybouncy.net>
2009-06-12 23:42:05 -0400

    new page

---
+++ modified_rowing.php
@@ -0,0 +1,153 @@
+
+<? 
+$title = "Modified Rowing (Mods)";
+include("header.inc");
+?>
+<div id="main">
+<h1> <?= $title ?> </h1>
+
+<p>
+ARC Junior rowers in 6th-8th grade may participate with the ARC Modified Team
+beginning in the Spring of 2009. No prior rowing experience is required. We
+intend to focus on the creation of a fun and safe opportunity to learn the
+sport of rowing and the basics of competition.   Modified Rowing is anticipated
+to grow, and more opportunities for competition will arise.  Until then this
+program may be thought of as a gateway for rowing hopefuls.
+</p>
+
+<p>
+The Modified Program is a first step into competitive rowing.  Consideration of
+modified rowers needs and expectations will be given.  Rowers will emerge with
+a good foundation of skill and knowledge.  Highly competitive Modified Rowers
+(MODS) may be eligible to row on the High School Team in 8th grade at the
+discretion of the Coach. Skill development in MODS will emphasize safety,
+technique, coordination, synchronization, flexibility, balance and strength.
+</p>
+
+<p>
+Because Modified rowing is developing, regatta schedules have not yet been
+arranged. However, it is anticipated that regattas for modified rowers will be
+held on Friday afternoon.  These are fun events and will give these young
+athletes the opportunity to test their skills against their peers.  More
+information will be available after the season gets under way.
+</p>
+
+<h2>Requirements</h2>
+    
+<ul>
+    <li> Able to tread water for 10 minutes unsupported </li>
+    <li> Able to carry 30 + lbs a total of 200 yards. </li>
+    <li> Running shoes </li>
+    <li> A bag with spare warm clothing </li>
+    <li> Non-disposable water bottle </li>
+</ul>    
+    
+    
+
+<p>
+You will be expected to perform certain duties as part of your learning
+experience.  If you are unable or unwilling to perform them you will be asked
+to leave.  You have to be able to swim and, as a minimum, tread water for ten
+minutes unsupported.  Swim Tests for Spring MODS will be held at Shaker High
+School Pool on March 29th at 9AM.  However, by joining this program you are
+attesting to the fact you are an able swimmer. 
+</p>
+
+<p>
+You must have a reasonable amount of strength.  The average weight of 35 pounds
+will be carried by each rower about 250 yards two times each practice.
+Initially this can be difficult but does become easier as you do it more
+frequently.
+</p>
+
+<p>
+Proper clothes and non-disposable water bottles must be brought to each
+practice.   As a base layer a rower usually wears a t-shirt and a pair of
+spandex shorts or pants (think Under Armour).  No baggy clothing should be
+worn.  Running shoes are to be brought every day.  Extra pants, shirts,
+sweatshirts and socks are needed every day:  Fleece or other synthetic
+polyesters are best when possible.  In terms of warm clothes, pack extra. You
+will get wet at least some of the time times by splash or the rare but
+occasional swamping.  Early spring rowing can get cold when you do not wear the
+right clothes. You will be fine if you dress appropriately. ARC Rowing trou,
+tights, uni's and hoodies are available for purchase, but are not required of
+MODS. 
+</p>
+
+<h2> Safety </h2>
+    
+    
+
+<p>
+Safety is everyone's responsibility.  If you do not feel safe for any reason
+tell the coach.  Also tell your parents.  Please try to make the environment
+safe for your teammates. 
+</p>
+
+<p>
+Life jackets will not be worn.  You cannot row with a life jacket.   We do
+carry life jackets in our coaching launches at all times.  Should you end up in
+the water for any reason, you should stay with your rowing shell.  In the event
+a shell capsizes, stay with the shell and check to make sure all rowers in your
+crew have come to the surface.
+</p>
+    
+    
+
+<h2> Weather </h2>
+    
+    
+
+<p>
+We will conduct a practice unless you are informed otherwise.  Some days we
+cannot row due to wind or thunderstorms.  Land training will occur on such
+days. We will row in light rain if the water is safe. Always dress
+appropriately for the weather. Hats are very useful. Remember, it is usually
+colder by the river.
+</p>
+    
+<h2> Session Dates and Practice Times </h2>
+
+<dl>
+<dt> Spring Season</dt>
+<dd> 3/30/2009-5/30/2009 </dd>
+
+<dt> Practice </dt>
+<dd> M-TH 4-6PM </dd>
+
+<dt> Regattas </dt>
+<dd> Friday Afternoons</dd>
+
+<dt> MODS Championships</dt>
+<dd> May 30th</dd>
+</dl>
+
+
+<h2> Fees </h2>
+<p>
+$310 per person
+</p>    
+    
+
+<h2> Questions? </h2>
+    
+    
+
+<p>
+If you have general questions about the ARC modified rowing program, please
+call MODS Coordinator Donna Quinn Holloway 312.9199 or e-mail
+<a href="mailto:dquinn5@nycap.rr.com">dquinn5@nycap.rr.com</a>
+</p>
+
+<p>
+<a href="/docs/Spring_09_ARC_Junior_Registration_Form.doc">Registration and</a> <a href="/docs/Spring_09_Order_Form.pdf">Clothing Order</a> forms are available on the website.
+</p>
+
+<p>
+If you have coaching questions, please call Head Coach, Drue Spallholz at
+312.7485 or e-mail druespalz@hotmail.com. 
+</p>
+
+
+</div>
+<? include("footer.inc"); ?>
