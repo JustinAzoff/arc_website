b7e057783a451eff939bdf80ee0cbf797edfd1e7
Justin Azoff <justin@bouncybouncy.net>
2009-06-27 13:43:24 -0400

    added 404 page

---
+++ 404.php
@@ -0,0 +1,14 @@
+
+<? 
+$title = "Not Found";
+include("header.inc");
+?>
+<div id="main">
+<h1> <?= $title ?> </h1>
+
+<p>
+Sorry, the page you have tried to access does not exist.
+</p>
+
+</div>
+<? include("footer.inc"); ?>
