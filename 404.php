307d07cb5762e8ed83bb75628b1227ec70b6f4b3
Justin Azoff <justin@bouncybouncy.net>
2009-07-11 17:46:24 -0400

    remove header links that no longer exist, update 404 page

--- 404.php
+++ 404.php
@@ -1,14 +1,16 @@
 
 <? 
-$title = "Not Found";
+$title = "404 Not Found";
 include("header.inc");
 ?>
 <div id="main">
 <h1> <?= $title ?> </h1>
 
 <p>
-Sorry, the page you have tried to access does not exist.
-</p>
+Sorry, the page you have tried to access does not exist.  You can use the menu
+at the left to find the page you are looking for.  If you still can't find what
+you are looking for, <a href="contact.php">Contact Us</a> and we will be glad
+to answer any questions you may have.</p>
 
 </div>
 <? include("footer.inc"); ?>
