55d6e5fc2f4c9a8fe1690e5ba1343b804883d0bc
Justin Azoff <justin@bouncybouncy.net>
2009-07-18 18:17:47 -0400

    add head of the hudson pages

---
+++ hoth.php
@@ -0,0 +1,65 @@
+
+<? 
+$title = "Head of the Hudson Regatta";
+include("header.inc");
+?>
+<div id="main">
+<h1> <?= $title ?> </h1>
+
+<h2>2009 Head of the Hudson will take place on Saturday October 3rd</h2>
+<p>
+Contact <a href="mailto:jrtauzel@yahoo.com">jrtauzel@yahoo.com</a> for all
+inquiries regarding 2009 registration and scheduling
+</p>
+
+<h2>The Head of the Hudson Regatta</h2>
+<p>
+Each year in the fall, the Albany Rowing Center hosts the Head of the Hudson
+Regatta. Typical of other "head" races that are held throughout the country in
+the fall, the Head of the Hudson is a three-mile race "against the clock." The
+Head of the Hudson is our premier event, bringing together hundreds of
+competitors from high schools, colleges and clubs from the Capital District,
+other areas of New York and surrounding states. The regatta consists of thirty
+or more different race events, in which adults, juniors and recreational
+rowers, both novice and experienced, may compete. Competing rowers begin at a
+start point three miles north of the Albany Rowing Center boathouse on the
+Hudson River in Albany and, one after the other, race south to finish just past
+the boathouse. The best finishing time in each race event determines the
+winner. Gold and silver medals are awarded to the first and second-place
+finishers in each race event, and special trophies are also awarded to winning
+teams of certain sponsored races. An All-Points Trophy is awarded at the end of
+the regatta to the team with the most points overall, based on placement in
+each race event.
+</p>
+<p>
+The regatta is both ARC's coveted home regatta as well as the focal point for
+our fall fundraising drive. Founded in 1985, the Albany Rowing Center, located
+on the Hudson River in downtown Albany, is committed to providing recreational
+and competitive rowing opportunities for both youth and adults. We use
+membership dues and equipment fees to meet operational expenses and coaching
+salaries. We rely on fundraising activities and sponsorships to cover the costs
+of the events that we host as well as to contribute toward special equipment
+needs such as new oars or boats. The Albany Rowing Center is a 501(c)(3)
+not-for-profit organization. Contributions are tax-deductible to the extent
+allowed by law.
+<a href="/docs/head of the hudson brochure2009.pdf">Download a fundraising brochure.</a>
+</p>
+
+<h2>2008 Head of the Hudson</h2>
+<ul>
+    <li><a href="https://www.regattacentral.com/index.jsp?tab=regattas&amp;section=all_entries&amp;id=798">Entries</a></li>
+    <li><a href="http://www.regattacentral.com/regatta/results/798.pdf">Results</a></li>
+    <li><a href="hoth_2008_sponsors.php">Sponsors</a></li>
+</ul>
+
+<p>
+The 22nd Head of the Hudson Regatta was held on Saturday, September 27th, a day
+when the weathermen predicted heavy rain all afternoon that never materialized.
+Twenty-two rowing clubs from 18 cities and 5 states competed in the regatta.
+While this year's regatta had fewer race entries than 2007, there was greater
+diversity in the clubs that attended including more collegiate rowers and
+several new high school entries.
+</p>
+
+</div>
+<? include("footer.inc"); ?>
---
+++ hoth_2008_sponsors.php
@@ -0,0 +1,81 @@
+
+<? 
+$title = "HoH 2008 Sponsors and supporters";
+include("header.inc");
+?>
+<div id="main">
+<h1> <?= $title ?> </h1>
+
+<h2> Regatta Sponsors</h2>
+<ul>
+    <li><a href="http://www.albany.org">The City of Albany</a></li>
+
+    <li><a href="http:///www.stpetershealthcare.org">St. Peter's Cardiac &amp; Vascular Center</a>
+        - The Science of Medicine. The Compassion to Heal.</li>
+</ul>
+
+<h2>Race Sponsors</h2>
+<ul>
+    <li><a href="http://www.albany.org">Hon. Gerald D. Jennings, Mayor, City of Albany</a></li>
+
+    <li><a href="http://www.evansale.com">Albany Pump Station</a>
+        - Serious beer. Delicious food. Incredible space.</li>
+
+    <li><a href="www.allu.com">AllU Activewear</a>
+        - Making someone's favorite shirt since 1986.</li>
+
+    <li>Ambrosino Design, Inc
+        - We'll help you make that first impression the right one.</li>
+
+    <li>ARC Junior Team - Driven</li>
+
+    <li><a href="http://www.cdphp.com">CDPHP</a>
+        - A plan for life</li>
+
+    <li><a href="http:///www.clarionhotelalbany.com">Clarion Hotel of Albany</a>
+        - Visit Scenarios Restaurant at the Clarion. A relaxed and comfortable way to end your day.</li>
+
+    <li><a href="http://www.closetworksny.com">Closet Works of New York, Inc.</a>
+        - Organize Your Life</li>
+
+    <li><a href="http://www.troyspa.com">Clinical Dimensions, LLC</a>
+        - A Health &amp; Beauty Spa in Historic Troy</li>
+
+    <li><a href="http://www.downtownalbany.org">Downtown Albany Business Improvement District</a>
+        - Downtown Albany - Worth Discovering Since 1609</li>
+
+    <li> The Evening Edition - ARC's Evening Rowing Programs
+        - Go ARC! What would we do without you?</li>
+
+    <li><a href="http://www.hrfmlaw.com">Heslin Rothenberg Farley &amp; Mesiti PC</a>
+        - Protecting new ideas - from concept to commercialization</li>
+
+    <li>The Mom posse
+        - In Memory of Betsy Owens</li>
+    <li><a href="http://www.northeasternairquality.com">Northeastern Air Quality, Inc.</a>
+        - Assuring peak building performance for industrial, commercial,
+          retail, health care and academic facilities.</li>
+
+    <li><a href="http://www.polsinello.com">Polsinello Fuels, Inc.</a>
+        - TLC=Total Lower Cost, over 54 years in business.</li>
+
+    <li><a href="http://www.riverbankpower.com">Riverbank Power</a>
+        - An innovative renewable power generation company</li>
+
+    <li><a href="http://www.stewartsshops.com">Stewart's Shops</a>
+        - We are closer to you. </li>
+</ul>
+
+<h2>Supporters</h2>
+<ul>
+    <li>Patricia Dowd</li>
+    <li>Rich and Louise Farrell</li>
+    <li>Frank Kuwik</li>
+    <li>Dr. Bill Primomo</li>
+    <li>Charles Touhey</li>
+    <li><a href="http://www.modernpress.com">Modern Press</a> 
+        - PRINT - COPY - DESIGN</li>
+</ul>
+
+</div>
+<? include("footer.inc"); ?>
