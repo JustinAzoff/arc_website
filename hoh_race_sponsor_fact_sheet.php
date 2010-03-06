9a43dc6b227365f2b99a6555981a18252c871c18
Justin Azoff <justin@bouncybouncy.net>
2010-03-06 11:59:37 -0500

    update hoh sponsor page for 2010 remove the date from the url setup a redirect from the old 2009 page

---
+++ hoh_race_sponsor_fact_sheet.php
@@ -0,0 +1,222 @@
+
+<? 
+$title = "Sponsor, Supporter and Advertiser Fact Sheet";
+include("header.inc");
+?>
+<div id="main">
+<h1> <?= $title ?> </h1>
+
+<p>
+We invite you to be a sponsor, a supporter or an advertiser in support of The
+24th Annual Head of the Hudson Regatta Saturday, September 25, 2010
+</p>
+
+<h2> Regatta Sponsor - $2000 or more </h2>
+
+<h2> Race Sponsorship Levels </h2>
+<ul>
+    <li> Race Sponsors - Trophy Level - $500 - $1000 </li>
+    <li> Race Sponsors - Medal Level - $350 - $499   </li>
+    <li> Race Sponsors - Ribbon Level - $250 - $349  </li>
+</ul>
+
+<h2> Regatta Supporters </h2>
+<ul>
+    <li> Regatta Supporters - $50 to $249 </li>
+</ul>
+
+<h2> Advertisers </h2>
+<ul>
+    <li> Half-Page Ad - $50 </li>
+    <li> Full-Page Ad - $75 </li>
+</ul>
+
+<h2>The Head of the Hudson Regatta</h2>
+<p>
+Each year in the fall, the Albany Rowing Center hosts the Head of the Hudson
+Regatta.  Typical of other "head" races that are held throughout the country in
+the fall, the Head of the Hudson is a three-mile race "against the clock."  The
+regatta consists of thirty or more different race events, in which adults,
+juniors and recreational rowers, both novice and experienced, may compete.
+Competing rowers begin at a start point three miles north of the Albany Rowing
+Center boathouse on the Hudson River in Albany and, one after the other, race
+south to finish just past the boathouse.  The best finishing time in each race
+event determines the winner.  Gold and silver medals are awarded to the first
+and second-place finishers in each race event, and special trophies are also
+awarded to winning teams of certain sponsored races.  An All-Points Trophy is
+awarded at the end of the regatta to the team with the most points overall,
+based on placement in each race event.  
+</p>
+
+
+<h2>Albany Rowing Center</h2>
+Founded in 1985, the Albany Rowing Center, located on the Hudson River in
+downtown Albany, is a 501(c)(3) not-for-profit organization committed to
+providing recreational and competitive rowing opportunities for both youth and
+adults.  We use membership dues and equipment fees to meet operational expenses
+and to purchase equipment.  We rely on fund raising activities and sponsorships
+to cover the costs of the events that we host.   
+</p>
+
+<h2> Events </h2>
+<p>
+We conduct several rowing events each year.  We celebrate National Learn to Row
+Day in early June, and conduct a Community Rowing Challenge on the same day.
+We host a number of scrimmages against other crews during the course of our
+summer season.  We host the Head of the Hudson Regatta each fall at the end of
+September.  The Head of the Hudson is our premier event, bringing together
+hundreds of competitors from high schools, colleges and clubs from the Capital
+District, other areas of New York and surrounding states.  This year's Head of
+the Hudson Regatta is scheduled for Saturday, September 27.  The regatta will
+take place from approximately 7:00 am to 2:00 pm, and we plan to run more than
+thirty different race events during the course of the day.
+</p>
+
+<h2> Race Sponsorships for Head of the Hudson </h2>
+<p>
+As we have done for the past several years, we are inviting businesses and
+individuals to be sponsors of this year's Head of the Hudson.  Our goal is to
+name a sponsor for every race event.  We are offering three levels of
+sponsorship - Trophy Level, Medal Level and Ribbon Level.  At each level, we
+will give recognition to our contributors in the form of various sponsor
+benefits. 
+</p>
+
+<h3> $500 to $1000 - Trophy Level Race Sponsors </h3>
+<p>
+Individuals/businesses contributing from $500 to $1000 will be named Race Sponsors at the Trophy Level.    
+Trophy Level Race Sponsor benefits include - </p>
+
+<ul>
+    <li> Named as individual race event sponsor </li>
+    <li> Thanked in the regatta program </li>
+    <li> Full-page ad in the regatta program </li>
+    <li> Named as race sponsor on the schedule of events </li>
+    <li> Thanked on the Albany Rowing Center website </li>
+    <li> Albany Rowing website link provided to sponsor's website </li>
+    <li> Publicly thanked during the course of the regatta </li>
+    <li> Sponsor logo printed on event t-shirt </li>
+    <li> Sponsor receives one complimentary event t-shirt </li>
+    <li> Invited to set up information table at the regatta </li>
+    <li> Invited to award gold and silver medals to first and second-place finishers of sponsored race event </li>
+    <li> Special trophy will be made with sponsor's name on it </li>
+    <li> Invited to award special trophy to winning team of sponsored race </li>
+</ul>
+
+<h3> $350 to $499 - Medal Level Race Sponsors</h3>
+<p>
+Individuals/businesses contributing from $350 to $499 will be named Race Sponsors at the Medal Level.  
+Medal Level Race Sponsor benefits include - </p>
+<ul>
+    <li> Named as individual race event sponsor </li>
+    <li> Thanked in the regatta program </li>
+    <li> Half-page ad in regatta program </li>
+    <li> Named as race sponsor on the schedule of events </li>
+    <li> Thanked on the Albany Rowing Center website </li>
+    <li> Albany Rowing website link provided to sponsor's website </li>
+    <li> Publicly thanked during the course of the regatta </li>
+    <li> Sponsor logo printed on event t-shirt </li>
+    <li> Sponsor receives one complimentary event t-shirt </li>
+    <li> Invited to set up information table at the regatta </li>
+    <li> Invited to award gold and silver medals to first and second-place </li>
+    <li> finishers of sponsored race event </li>
+    <li> Special trophy will be made with sponsor’s name on it </li>
+    <li> Invited to award special trophy to winning team of sponsored  race event  </li>
+</ul>
+
+
+<h3> $250 to $349 - Ribbon Level Race Sponsors </h3>
+<p> Individuals/businesses contributing from $250 to $349 will be named Race Sponsors at the Ribbon Level.
+Ribbon Level Race Sponsor benefits include -  </p>
+
+    <li> Named as individual race event sponsor </li>
+    <li> Thanked in the regatta program </li>
+    <li> Half-page ad in regatta program </li>
+    <li> Named as a race sponsor on the schedule of events </li>
+    <li> Thanked on the Albany Rowing Center website </li>
+    <li> Albany Rowing website link provided to sponsor's website </li>
+    <li> Publicly thanked during the course of the regatta </li>
+    <li> Sponsor logo printed on event t-shirt </li>
+    <li> Sponsor receives one complimentary event t-shirt </li>
+    <li> Invited to award gold and silver medals to first and second-place finishers of sponsored race event </li>
+</ul>
+
+<h3> Regatta Supporters </h3>
+<p>
+Individuals/businesses contributing any amount from $50 up to $249 will be
+named Regatta Supporters.  Regatta Supporter benefits include -  </p>
+
+<ul>
+    <li> Thanked in regatta program </li>
+    <li> Thanked on the Albany Rowing Center website </li>
+    <li> Albany Rowing provides website link to supporter's website </li>
+    <li> Publicly thanked at the regatta </li>
+    <li> Supporter receives one complimentary event t-shirt  </li>
+</ul>
+
+<h2> Note </h2>
+<p>
+Businesses that make donations to the regatta of goods or services that
+have a quantifiable dollar value will be named in the appropriate sponsor or
+supporter category and will receive the associated benefits.
+</p>
+
+<h2> Advertisers </h2>
+<p>
+Individuals may place messages in the regatta program at a cost of $50 for a
+half-page message or $75 for a full-page message.  Businesses may advertise in
+the regatta program at the same cost - $50 for a half-page ad and $75 for a
+full-page ad.  Advertisers will receive one complimentary event t-shirt. 
+</p>
+
+<h2> Your Audience </h2>
+<p>
+By sponsoring a race at Head of the Hudson, your name will be seen by the
+competing rowers, their coaches, their families and friends, the race
+officials, the various organizations that provide support assistance for the
+regatta and the other individuals and businesses that have also chosen to
+sponsor races and support the regatta.  Together, these people come from the
+Capital District, other regions of New York State and several other states as
+well.    
+</p>
+
+<h2> Making a Contribution </h2>
+<p>
+Individuals and businesses interested in sponsoring a Head of the Hudson race
+event, making a supporting contribution or advertising in the regatta program
+should contact Christina Dowd, Head of the Hudson Fund Raising Co-Chair, by
+email at <a href="mailto:cdowd001@nycap.rr.com">cdowd001@nycap.rr.com</a>, by
+phone at (518) 402-8976, or by mail to Christina Dowd, Albany Rowing Center, PO
+Box 857, Albany, NY 12201 - 0857.  By no later than the beginning of August, we
+will request a check payable to the Albany Rowing Center, and will also ask for
+material and information that relates to sponsor and supporter benefits, such
+as ad copy and website address.
+</p>
+
+<h2>Significant Contributions</h2>
+<p>
+The Albany Rowing Center will be pleased to establish relationships with
+businesses, organizations or individuals that choose to make significant
+contributions greater than two-thousand dollars.  Specific benefits will be
+discussed with such supporters on an individual basis at the time a pledge or
+donation is made.  To make a significant contribution to Albany Rowing Center
+and to discuss associated benefits please contact Christina Dowd at
+<a href="mailto:cdowd001@nycap.rr.com">cdowd001@nycap.rr.com</a> or (518) 452-1123.  
+</p>
+
+<h2> Questions </h2>
+<p>
+Questions may be directed to Christina at the email above. 
+</p>
+
+<h2> Thank you for your support! </h2>
+<p>
+The Albany Rowing Center is a 501(c)(3) not-for-profit organization.
+</p>
+<p>
+Contributions are tax-deductible to the extent allowed by law. 
+</p>
+ 
+
+</div>
+<? include("footer.inc"); ?>
