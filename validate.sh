561764cb115fcdef31799eae5551d9e0ebc721b0
Justin Azoff <justin@bouncybouncy.net>
2010-03-07 20:35:23 -0500

    don't validate pages that are just redirections

--- validate.sh
+++ validate.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 for x in *.php; do
-    [ -e /tmp/$x.html ] || wget -c albanyrowingcenter.org/$x -O /tmp/$x.html;
+    [ -e /tmp/$x.html ] || grep -q header..location -i $x|| wget -c albanyrowingcenter.org/$x -O /tmp/$x.html;
 done
 for x in /tmp/*php.html;do
     validate  $x;
