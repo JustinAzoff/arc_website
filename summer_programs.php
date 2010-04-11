c4067ce868a4e93710acd963fdf9bad6570af371
Justin Azoff <justin@bouncybouncy.net>
2010-04-11 12:39:34 -0400

    updated for 2010

--- summer_programs.php
+++ summer_programs.php
@@ -1,30 +1,31 @@
 
 <? 
-$title = "Summer Camp 2009";
+$title = "Summer Camp 2010";
 include("header.inc");
 ?>
 <div id="main">
 <h1> <?= $title ?> </h1>
 
 <p>
-The Albany Rowing Center Learn to Row programs offer the experience of rowing
-to interested youth entering grades 6 and above. The Learn to Row Program
-permits young athletes to learn about the sport of rowing through
+The Albany Rowing Center Junior Summer Camp Program offers instruction in
+rowing  to interested youth entering grades 6 and above. The Learn to Row
+Program permits young athletes to learn about the sport of rowing through
 participation. Students will learn water and boat safety, stroke mechanics on
-the ergometer (rowing machine) and the basic techniques of sweep rowing. While
-the majority of time is spent on the water, there is also dry land training and
-education about boats and equipment.
+the ergometer (rowing machine) and the basic techniques of sweep rowing and
+sculling. While the majority of time is spent on the water, there is also
+fitness training on land and education about boats and equipment. Students who
+wish to continue rowing may join the fall competitive rowing program.
 </p>
 
 <h2> Swim Test </h2>
 
 <p>
-The swim test can be taken at ANY POOL where a lifeguard is present. We do not
-administer swim tests at the boathouse. Please print out the
+The swim test can be taken at <em>any pool</em> where a lifeguard is present.
+We do not administer swim tests at the boathouse. Please print out the
 <a href="/docs/Junior Swim Test Certification Form.pdf">Swim Test Certification</a>
 and have a lifeguard sign it.  You can either mail the form to Albany Rowing
-Center OR bring it to your first day of class. You may not be boated without
-the swim test.
+Center <em>or</em> bring it to your first day of class. You may not be boated
+without the swim test.
 </p>
 
 <h2> Requirements </h2>
@@ -40,40 +41,41 @@ the swim test.
 <h2> What to Bring... </h2>
 <ul> 
     <li> Sunscreen </li>
-    <li> Refillable water bottle </li>
-    <li> Change of clothing (sneakers, socks, and snacks are all necessary items to bring) </li>
+    <li> Drinking water in a reusable water bottle </li>
+    <li> A snack for mid-morning break </li>
+    <li> Change of clothing (sneakers, socks, shirt, shorts) </li>
 </ul>
 
 <h2> Schedule and Time </h2>
 <p>
-Camp Time is 9 am - 11:30 am, Monday through Friday 
+Camp Time is 9 am - 12:00 am, Monday through Friday 
 </p>
 
 <ul>
-    <li> Session 1 - June 29th - July 10th </li>
-    <li> Session 2 - July 13th - July 24th </li>
-    <li> Session 3 - July 27th - August 7th </li>
+    <li> Session 1 - June 28th - July 9th </li>
+    <li> Session 2 - July 12th - July 23th </li>
+    <li> Session 3 - July 26th - August 6th </li>
 </ul>
 
 <h2> Fees </h2>
 <p>
-$195 per session. (There is a discount for a child in more than one session: $320 for 2 and $420 for all 3)
+$250 per session (A discount will be offered for those who would like to continue for a second or third session.)
 </p>
 
 <h2> Registration </h2>
 
 <p>
-Registration for 2009 Junior Learn to Row at Albany Rowing Center is simple.
-Please complete the
-<a href="/docs/ARC Junior SUMMER CAMP Registration Packet.pdf">2009 Junior Learn to Row Reqistration Packet</a> and mail to:
+Registration for Junior Learn to Row at Albany Rowing Center is simple. Forms and other information will be available shortly. For information on this program please contact
+<a href="mailto:juniorsrowing@albanyrowingcenter.org">juniorsrowing@albanyrowingcenter.org</a> or
+<a href="mailto:info@albanyrowingcenter.org">info@albanyrowingcenter.org</a>
+
 </p>
 
-
 <pre>
-Organization of Adirondack Rowers and Scullers
-Junior Registration
-Albany Rowing Center C/O Bierman
-111 Read Road, Glenmont, NY 12077 
+    Organization of Adirondack Rowers and Scullers
+    Junior Registration
+    Albany Rowing Center C/O Bierman
+    111 Read Road, Glenmont, NY 12077 
 </pre>
 
 </div>
