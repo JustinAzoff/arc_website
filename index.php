fe32054d7c01a0834bf95da060e6d7ad93dac0e2
Justin Azoff <justin@bouncybouncy.net>
2010-09-18 17:51:21 -0400

    add initial slideshow stuff

--- index.php
+++ index.php
@@ -4,15 +4,59 @@ include("header.inc");
 ?>
 <div id="main">
 
-<h1> Welcome to Albany Rowing Center! </h1>
+
+<h1>About Us</h1>
+<?php insert_slideshow($all_slideshow); ?>
 <p>
-Albany Rowing Center (ARC) is a not-for-profit organization that fosters
-interest in and provides opportunity for rowing in New York's Capital
-District.
+<strong>Welcome to Albany Rowing Center!</strong>
+<p>
+Founded in 1985, the Albany Rowing Center, located on the Hudson River in
+downtown Albany, is committed to providing recreational and competitive rowing
+opportunities for both youth and adults.  Albany Rowing Center (ARC) is a
+not-for-profit organization that fosters interest in and provides opportunity
+for rowing in New York's Capital District.  ARC rowers represent all ages (from
+middle school students on up) and a wide range of rowing abilities (from novice
+to experienced).
 </p>
 <p>
-ARC rowers represent all ages (from middle school students on up) and a
-wide range of rowing abilities (from novice to experienced). 
+We conduct several rowing events each year. We celebrate National Learn to Row
+Day in early June, and conduct a Community Rowing Challenge on the same day. We
+host a number of scrimmages against other crews during the course of our summer
+season. We host the Head of the Hudson Regatta each fall at the end of
+September. The Head of the Hudson is our premier event, bringing together
+hundreds of competitors from high schools, colleges and clubs from the Capital
+District, other areas of New York and surrounding states.  </p>
+
+<h1>News</h1>
+
+<h2>Changes to this year's Head of the Hudson Regatta</h2>
+<p>
+This year, the twenty fourth in the series, we've made some changes to the
+event. We have shortened the usual 3 mile race to be just under 2 miles (3.2K)
+and it is much more spectator friendly, with viewing possible at every point
+along the course. Competing rowers begin at a start point just north of the I
+90 bridge, upstream of the Albany Rowing Center boathouse on the Hudson River
+and, one after the other, race south to finish at the amphitheater at the
+Corning Preserve.  <a href="hoh.php">Read More about the changes to the Head of the Hudson</a>
+</p>
+
+<h2>Our New Site! </h2>
+<p>
+Thank you to everyone who helped out in making this new site possible!  Special thanks to:
+</p>
+
+<p>
+Kim Stock of Kim Stock Photography for coming to one of our evening practices
+and shooting beautiful photos for the new site, as well as providing us with
+the graphic design associated with this year's Head of the Hudson Regatta.
+</p>
+
+<p>
+Julie Lewis, for taking most of the videos linked in the “Media” section in the
+YouTube player, and for providing us a link to her blog <a href="http://ghostcoaching.blogspot.com/">Eat, Row, Sleep,Coaching Advice for Rowers and Scullers.</a></p>
+
+<p>
+And to all else who helped with providing photos and information to make this happen, Thank you!
 </p>
 
 <h1>State Champions!</h1>
@@ -22,7 +66,6 @@ ARC's Girls Varsity 8 just won the NYS championship!</a>  They came in first
 beating out all Girls Varsity 8's in the State.  They will now move onto the
 National Championship in Ohio in June.  
 <a href="http://poststar.com/sports/article_d2ac0300-5beb-11df-90df-001cc4c002e0.html?mode=story">
-<img border=0 align="center" src="2010 State Champs.jpg" />
 </a>
 </p>
 
