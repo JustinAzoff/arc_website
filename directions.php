0887362d0b219f253118fe56e1fcdd61f8939624
Justin Azoff <justin@bouncybouncy.net>
2010-09-22 22:30:17 -0400

    put map in sidebar

--- directions.php
+++ directions.php
@@ -2,6 +2,13 @@
 $title = "Directions";
 include("header.inc");
 ?>
+<div id="sidebar">
+
+<h2>Map</h2>
+
+<iframe width="480" height="480" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps/ms?daddr=Albany+Rowing+Center+%4042.656892,-73.741699&amp;geocode=&amp;dirflg=&amp;saddr=Albany,+ny&amp;hl=en&amp;dq=albany+rowing+center&amp;ie=UTF8&amp;t=h&amp;msa=0&amp;msid=112436428230627761706.00046bcdf718b4083d46b&amp;ll=42.658533,-73.743989&amp;spn=0.007574,0.013733&amp;z=16&amp;output=embed"></iframe><br /><small>View <a href="http://maps.google.com/maps/ms?daddr=Albany+Rowing+Center+%4042.656892,-73.741699&amp;geocode=&amp;dirflg=&amp;saddr=Albany,+ny&amp;hl=en&amp;dq=albany+rowing+center&amp;ie=UTF8&amp;t=h&amp;msa=0&amp;msid=112436428230627761706.00046bcdf718b4083d46b&amp;ll=42.658533,-73.743989&amp;spn=0.007574,0.013733&amp;z=16&amp;source=embed" style="color:#0000FF;text-align:left">Albany Rowing Center</a> in a larger map</small>
+
+</div>
 <div id="main">
 <h1> Directions </h1>
 <p>
@@ -37,10 +44,5 @@ Get off almost immediately at Clinton Ave. exit.
 Stay right and follow the sign for Colonie St. Proceed straight through the light.
 Take the first left onto Erie St. Enter the parking lot straight ahead.
 </p>
-
-<h2>Map</h2>
-
-<iframe width="640" height="480" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps/ms?daddr=Albany+Rowing+Center+%4042.656892,-73.741699&amp;geocode=&amp;dirflg=&amp;saddr=Albany,+ny&amp;hl=en&amp;dq=albany+rowing+center&amp;ie=UTF8&amp;t=h&amp;msa=0&amp;msid=112436428230627761706.00046bcdf718b4083d46b&amp;ll=42.658533,-73.743989&amp;spn=0.007574,0.013733&amp;z=16&amp;output=embed"></iframe><br /><small>View <a href="http://maps.google.com/maps/ms?daddr=Albany+Rowing+Center+%4042.656892,-73.741699&amp;geocode=&amp;dirflg=&amp;saddr=Albany,+ny&amp;hl=en&amp;dq=albany+rowing+center&amp;ie=UTF8&amp;t=h&amp;msa=0&amp;msid=112436428230627761706.00046bcdf718b4083d46b&amp;ll=42.658533,-73.743989&amp;spn=0.007574,0.013733&amp;z=16&amp;source=embed" style="color:#0000FF;text-align:left">Albany Rowing Center</a> in a larger map</small>
-
 </div>
 <? include("footer.inc"); ?>
