75383308df3adc4cf680e7aaecdfbc50a671920e
Justin Azoff <justin@bouncybouncy.net>
2011-01-28 08:06:13 -0500

    more updates for the ice breaker challenge

--- ice_breaker_challenge.php
+++ ice_breaker_challenge.php
@@ -6,6 +6,17 @@ include("header.inc");
 <div id="main">
 <h1> <?= $title ?> </h1>
 
+<p>
+The Ice Breaker Challenge will be a first of its kind for the Capital District
+running/walking road race challenging runners and walkers against rowers from
+the Albany Rowing Center. Runners/walkers will travel along the Corning
+Preserve Bicycle Path while rowers from the Albany Rowing Center will race
+along side in shells on the Hudson River. Come join us for this unique and
+exciting event on Saturday, March 26, 2011 beginning at 9:00 am at the Albany
+Rowing Center Boathouse in the Corning Preserve.
+</p>
+
+
 <dl>
 <dt> When: </dt>
 <dd> Saturday March 26, 2011 at 9:00 am </dd>
