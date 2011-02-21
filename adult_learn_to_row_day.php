1c458bf8dca9e32a519b7ab9f44103ddbbf1aa8f
Justin Azoff <justin@bouncybouncy.net>
2011-02-21 13:19:33 -0500

    new learn to row day page

---
+++ adult_learn_to_row_day.php
@@ -0,0 +1,48 @@
+
+<? 
+$title = "2011 Adult Learn to Row Day";
+include("header.inc");
+?>
+<div id="main">
+<h1> <?= $title ?> </h1>
+<p>
+Learn to Row Day will be held on Saturday, April 23rd 2011. Learn to Row Day is
+free and open to adults 18 years and older. It is an opportunity for anyone
+interested in learning about rowing to get a hands-on introduction to the sport
+and find out about ARC’s rowing programs. Members of the community may drop in
+anytime during the scheduled activities and receive the following:
+
+<ul>
+<li>a tour of the boathouse and demonstration of our rowing equipment</li>
+<li>instruction on one of ARC’s rowing machines to learn the basics of the rowing stroke</li>
+<li>a chance to actually go out on the Hudson River in an eight person racing
+shell and row with other novices and experienced ARC rowers.</li>
+</ul>
+</p>
+
+The entire process takes approximately 45 minutes depending on how many people are
+interested. All participants will be asked to sign in and fill out a waiver.
+
+<dl>
+<dt>When</dt>
+<dd>Saturday April 23rd 2011 anytime between 8:30 AM and 12:30 PM</dd>
+
+<dt>Where</dt>
+<dd>The City of Albany Boat Shed, Hudson River Corning Preserve (<a href="/directions.php">Directions</a>)</dd>
+
+<dt>What to Wear</dt>
+<dd>Comfortable clothes suitable for the weather. Bring drinking water. Please
+leave backpacks or other belongings in your car as there is no room for them in
+the boat.</dd>
+<dt>Contact</dt>
+<dd><a href="mailto:info@albanyrowingcenter.org">info@albanyrowingcenter.org</a></dd>
+</dl>
+
+<h2>Documents</h2>
+<ul>
+<li><a href="/missing">Learn to Row Day flyer</a></li>
+<!--<li><a href="/docs/ARC2011 info brochure.pdf">Albany Rowing Center brochure</a></li> -->
+</ul>
+
+</div>
+<? include("footer.inc"); ?>
