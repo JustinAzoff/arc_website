955757f600ef42571153e0ac926313afaabdb42f
Justin Azoff <justin@bouncybouncy.net>
2011-02-21 12:54:52 -0500

    text for new adult learn to row page

--- adult_learn_to_row.php
+++ adult_learn_to_row.php
@@ -4,20 +4,22 @@ $header_image_tag="adults";
 include("header.inc");
 ?>
 <div id="main">
-<h1> Adult Learn to Row 2010 </h1>
+
+<h2>Adult Learn to Row</h2>
 
 <p>
 This program is for adults interested in learning the basic techniques and
 styles of sculling and sweep rowing. The program is a low impact sport
-available to adults of all athletic levels. We offer five evening sessions of
-two weeks each. The two week(10 day) program includes both instruction on land
-and learning basic rowing technique on the water. On water activities are
-dependent on weather and river conditions. After completing the basic two week
-program, those interested in continuing to row will join the adult novice
-program.
+available to adults of all athletic levels. Each monthly Learn to Row session
+consists of four weeks of rowing three nights per week. The twelve day program
+includes both instruction on land and learning basic rowing technique on the
+water. On water activities are dependent on weather and river conditions. After
+completing the basic four week program, those interested in continuing to row
+can take a second Learn to Row session or advance to one of our regular adult
+rowing programs, at the discretion of the coach.
 </p>
 
-<h2> Prerequisite </h2>
+<h2>Prerequisite</h2>
 
 <p>
 You must be able to swim in order to participate in any ARC program.
@@ -29,50 +31,41 @@ must view and understand the USRowing Safety video which is shown on the first
 night of practice.
 </p>
 
-<h2> Session Dates and Practice Times </h2>
-<p>
-Evenings: Monday through Friday at 6:30 - 8:00 p.m.  Make up dates will be
-scheduled for holidays and weather cancellations. Two on land days are optional
-but participants are encouraged to attend each night.  Membership Dues are $175
-for the 10 day session. 
-</p>
+<h2>Session Dates and Practice Times</h2>
 
+<p>
+Evenings: Monday, Wednesday and Thursday at 6:30 - 8:00 p.m. Make up dates may
+be scheduled for holidays and weather cancellations at the discretion of the
+coach.  Participation Fee is $200 for the 12 day session. Morning sessions may
+be offered if there is sufficient interest (12 participants).
+</p>
+<p>
 <table class="thin">
 <tr> <th>Session</th> <th>Dates</th> </tr>
-<tr> <td>1</td> <td> May 10th - May 21st </td> </tr>
-<tr> <td>2</td> <td> June 1st - June 11th </td> </tr>
-<tr> <td>3</td> <td> June 21st - July 2nd </td> </tr>
-<tr> <td>4</td> <td> July 12th - July 23rd </td> </tr>
-<tr> <td>5</td> <td> Canceled </td> </tr>
+<tr> <td>1</td> <td> April 4    - April 28 </td> </tr>
+<tr> <td>2</td> <td> May 2      - May 26   </td> </tr>
+<tr> <td>3</td> <td> June 6     - June 30  </td> </tr>
+<tr> <td>4</td> <td> July 5     - July 28  </td> </tr>
 </table>
-
-<h2> Coach </h2>
-
-<p> Jenny Wiegert </p>
-
-<h2> Questions ? </h2>
-
-<p>
-Please contact
-<a href="mailto:learn2row@albanyrowingcenter.org">learn2row@albanyrowingcenter.org</a>
-</p>
-<p> <a href="/docs/LTR brochure.2010.04.pdf">Learn to Row Brochure</a> </p>
-
-    
-<h2> Registration </h2>
-    
-<p>
-Registration will be limited to 12 participants for each session. Register now
-to reserve your seat.  To register, please complete the <a href="/docs/2010%20Adult Learn to Row Packet.pdf">registration packet</a> and mail to: 
 </p>
 
-<pre>
-    Adult LTR Program
-    Albany Rowing Center
-    PO Box 857
-    Albany, NY 12201
+<h2>Coach</h2>
+<p>
+Evan Bollin
+</p>
+
+<h2>Questions?</h2>
+<p>
+Please contact learn2row@albanyrowingcenter.org
+</p>
+<h2>Registration</h2>
+
+Registration is generally limited to 12 participants for each session. Register now to
+reserve your seat. To register, please complete the registration packet and mail to the
+address indicated.
+
+
 </pre>
-
 
 </div>
 <? include("footer.inc"); ?>
