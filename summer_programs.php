506b33c7b8503990e8ed8c5af70d1867c14b5a8f
Justin Azoff <justin@bouncybouncy.net>
2011-03-20 21:38:13 -0400

    summer program updates

--- summer_programs.php
+++ summer_programs.php
@@ -6,82 +6,85 @@ include("header.inc");
 <div id="main">
 <h1> <?= $title ?> </h1>
 
+<h2>Junior Rowing Summer Camp</h2>
+
 <p>
-The Albany Rowing Center Junior Summer Camp Program offers instruction in
-rowing  to interested youth who have completed seventh grade. The Learn to Row
-Program permits young athletes to learn about the sport of rowing through
-participation. Students will learn water and boat safety, stroke mechanics on
-the ergometer (rowing machine) and the basic techniques of sweep rowing and
+Junior Rowing Summer Camp Program offers instruction in rowing to boys and
+girls who have finished 7th grade. Coach Evan Bollin teaches young athletes
+about the sport of rowing through participation in a fun and educational
+setting. Students will learn water and boat safety, stroke mechanics on the
+ergometer (rowing machine) and the basic techniques of sweep rowing and
 sculling. While the majority of time is spent on the water, there is also
-fitness training on land and education about boats and equipment. Students who
-wish to continue rowing may join the fall competitive rowing program.
+fitness training on land and education about boats and equipment.  Campers
+learn how to work as a team. Those who would like to continue rowing may join
+the fall competitive rowing program.
 </p>
 
-<h2> Swim Test </h2>
+<h2>Swim Test</h2>
 
 <p>
-The swim test can be taken at <em>any pool</em> where a lifeguard is present.
-We do not administer swim tests at the boathouse. Please print out the
-<a href="/docs/Junior Swim Test Certification Form.pdf">Swim Test Certification</a>
-and have a lifeguard sign it.  You can either mail the form to Albany Rowing
-Center <em>or</em> bring it to your first day of class. You may not be boated
-without the swim test.
+The swim test can be taken at <em>any pool</em>where a lifeguard is present. We
+do not administer swim tests at the boathouse. Please print out the Swim Test
+Certification and have a lifeguard sign it. You can either mail the form to
+Albany Rowing Center OR bring it to your first day of class. You may not be
+allowed to get in the boat without the swim test.
 </p>
 
-<h2> Requirements </h2>
-
+<h2>What We Need </h2>
 <ul>
     <li> Completed registration packet </li>
-    <li> Payment in full for 1st session </li>
+    <li> Payment in full for 1st session [Make check out to Albany Rowing Center.] </li>
     <li> Swim test certification </li>
-    <li> Must be entering Middle School </li>
-    <li> 6th grade or older </li>
 </ul>
 
-<h2> What to Bring... </h2>
-<ul> 
-    <li> Sunscreen </li>
-    <li> Drinking water in a reusable water bottle </li>
-    <li> A snack for mid-morning break </li>
-    <li> Change of clothing (sneakers, socks, shirt, shorts) </li>
+<h2> What to Bring </h2>
+<ul>
+    <li>Sunscreen, sunglasses and a hat</li>
+    <li>Drinking water in a reusable water bottle</li>
+    <li>A snack for mid-morning break</li>
+    <li>Change of clothing (sneakers, socks, shirt, shorts) and a towel</li>
+    <li>Any inhaler or other medication needed</li>
 </ul>
 
-<h2> Schedule and Time </h2>
+<h2>2011 Schedule and Time</h2>
 <p>
-Camp Time is 9 am - 12:00 am, Monday through Friday 
+Camp Time is 9 am - 12:00 am, Monday through Friday
 </p>
 
 <ul>
-    <li> Session 1 - June 28th - July 9th </li>
-    <li> Session 2 - July 12th - July 23th </li>
-    <li> Session 3 - July 26th - August 6th </li>
+    <li>Session 1 - June 27th - July 8th</li>
+    <li>Session 2 - July 11th - July 22nd</li>
+    <li>Race Camp! - July 25th - 29th.</li>
 </ul>
 
-<h2> Fees </h2>
+<h2>Fees</h2>
 <p>
-$250 per session (A discount will be offered for those who would like to continue for a second or third session.)
+$250 per session and $175 for Race Camp (Only campers who have rowed before
+will be enrolled in Race Week.)
 </p>
 
-<h2> Registration </h2>
+<h3>Campers Package</h3>
+<ul>
+    <li>1 session plus Race Camp = $400</li>
+    <li>2 sessions plus Race Camp = $600</li>
+</ul>
 
+<h2>Registration</h2>
 <p>
-Registration for Junior Learn to Row at Albany Rowing Center is simple.
-Please complete the <a href="/docs/2010 LTR Summer Camp Reg Form.pdf">2010 Summer Camp Registration Packet</a> and mail to:
+Registration for Junior Rowing Summer Camp at Albany Rowing Center is simple.
+Please complete the <a href="/docs/2011 LTR Summer Camp Reg Form.pdf">2011 Summer Camp Registration Packet</a> and mail to:
+Forms and other information will be available shortly. For information on this
+program please contact <a href="mailto:juniorsrowing@albanyrowingcenter.org">juniorsrowing@albanyrowingcenter.org</a>
+or <a href="mailto:info@albanyrowingcenter.org">info@albanyrowingcenter.org</a>
 </p>
+
 
 <pre>
-    Organization of Adirondack Rowers and Scullers
-    Junior Registration
-    Albany Rowing Center C/O Bierman
-    111 Read Road, Glenmont, NY 12077 
+    Albany Rowing Center c/o Laura Bierman
+    Junior Camp Registration
+    111 Read Road
+    Glenmont, NY 12077
 </pre>
-
-<p>
-For information on this program please contact
-<a href="mailto:juniorsrowing@albanyrowingcenter.org">juniorsrowing@albanyrowingcenter.org</a> or
-<a href="mailto:info@albanyrowingcenter.org">info@albanyrowingcenter.org</a>
-</p>
-
 
 </div>
 <? include("footer.inc"); ?>
